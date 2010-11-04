5f46ae235aeef35ca190d2f4f158e81e21d4f7cf
Federico Brubacher <fbrubacher@gmail.com>
2010-11-04 11:14:35 -0300

    some changes on init.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -195,3 +195,31 @@
 (server-start)
 ;; To customize the background color
 (set-face-background 'highlight-current-line-face "light red")
+(defvar no-easy-keys-minor-mode-map (make-keymap) 
+  "no-easy-keys-minor-mode keymap.")
+(let ((f (lambda (m)
+           `(lambda () (interactive) 
+              (message (concat "No! use " ,m " instead."))))))
+  (dolist (l '(("<left>" . "C-b") ("<right>" . "C-f") ("<up>" . "C-p")
+               ("<down>" . "C-n")
+               ("<C-left>" . "M-f") ("<C-right>" . "M-b") ("<C-up>" . "M-{")
+               ("<C-down>" . "M-}")
+               ("<M-left>" . "M-f") ("<M-right>" . "M-b") ("<M-up>" . "M-{")
+               ("<M-down>" . "M-}")
+               ("<delete>" . "C-d") ("<C-delete>" . "M-d")
+               ("<M-delete>" . "M-d") ("<next>" . "C-v") ("<C-next>" . "M-x <")
+               ("<prior>" . "M-v") ("<C-prior>" . "M-x >") 
+               ("<home>" . "C-a") ("<C-home>" . "M->")
+               ("<C-home>" . "M-<") ("<end>" . "C-e") ("<C-end>" . "M->")))
+    (define-key no-easy-keys-minor-mode-map
+      (read-kbd-macro (car l)) (funcall f (cdr l)))))
+(define-minor-mode no-easy-keys-minor-mode
+  "A minor mode that disables the arrow-keys, pg-up/down, delete
+  and backspace."  t " no-easy-keys"
+  'no-easy-keys-minor-mode-map :global t)
+(no-easy-keys-minor-mode 1)
+(progn
+  (setq cdt-dir "/Users/federicobrubacher/clojure/cdt")
+  (setq cdt-source-path "/Users/federicobrubacher/clojure/clojure-1.2.0/src/jvm:/Users/federicobrubacher/clojure/clojure-1.2.0/src/clj:/Users/federicobrubacher/clojure/clojure-contrib-1.2.0/src/main/clojure:")
+  (load-file (format "%s/ide/emacs/cdt.el" cdt-dir)))
+(global-linum-mode 1)
